039285c6c9a5096f81488d92324981e968a938c7
Harrison <hichyen1207@gmail.com>
2019-01-10 22:04:46 +0800

    add works

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def handle_message(event):
                             ]
                         ),
                         CarouselColumn(
-                            thumbnail_image_url='https://newsound.herokuapp.com/static/media/newsound-logo.88a4c9cb.png',
+                            thumbnail_image_url='https://i.imgur.com/CYcSKeP.jpg',
                             title='【New Sound Website】',
                             text='New Sound Website',
                             actions=[
@@ -104,7 +104,7 @@ def handle_message(event):
                             ]
                         ),
                         CarouselColumn(
-                            thumbnail_image_url='https://momofit.herokuapp.com/momofit/static/img/logo.png',
+                            thumbnail_image_url='https://i.imgur.com/wo3gJPi.jpg',
                             title='【momofit Website】',
                             text='momofit Website',
                             actions=[
