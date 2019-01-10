5bf9775147c8328f9c2804b138e9664ab1141b86
Harrison <hichyen1207@gmail.com>
2019-01-10 21:54:34 +0800

    add works

--- app.py
+++ app.py
@@ -82,7 +82,8 @@ def handle_message(event):
                 template=CarouselTemplate(
                     columns=[
                         CarouselColumn(
-                            thumbnail_image_url='https://www.smartone.com/services_and_apps/tchinese/Spotify.png',
+                            # thumbnail_image_url='https://www.smartone.com/services_and_apps/tchinese/Spotify.png',
+                            thumbnail_image_url='',
                             title='【Spotify Demo】',
                             text='',
                             actions=[
@@ -93,7 +94,8 @@ def handle_message(event):
                             ]
                         ),
                         CarouselColumn(
-                            thumbnail_image_url='https://newsound.herokuapp.com/static/media/newsound-logo.88a4c9cb.png',
+                            # thumbnail_image_url='https://newsound.herokuapp.com/static/media/newsound-logo.88a4c9cb.png',
+                            thumbnail_image_url='',
                             title='【New Sound Website】',
                             text='',
                             actions=[
@@ -104,7 +106,8 @@ def handle_message(event):
                             ]
                         ),
                         CarouselColumn(
-                            thumbnail_image_url='https://momofit.herokuapp.com/momofit/static/img/logo.png',
+                            # thumbnail_image_url='https://momofit.herokuapp.com/momofit/static/img/logo.png',
+                            thumbnail_image_url='',
                             title='【momofit Website】',
                             text='',
                             actions=[
